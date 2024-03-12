7a41ada419914a2b3da79885776e6000c1d86a78
Yush1nk1m <kys010306@sogang.ac.kr>
2024-03-12 14:42:43 +0900

    20240312

---
+++ Do_it_algorithm/Chapter08/P2251.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    return 0;
+}
