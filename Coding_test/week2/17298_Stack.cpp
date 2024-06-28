152d761efb0988c0c44251f946fea70b54754249
Yush1nk1m <kys010306@sogang.ac.kr>
2024-06-28 22:07:18 +0900

    20240628: 17298번 해결

---
+++ Coding_test/week2/17298_Stack.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, number;
+vector<int> nge;
+stack<pair<int, int>> stk;
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    nge.resize(N + 1, -1);
+    for (int index = 1; index <= N; index++) {
+        cin >> number;
+        while (!stk.empty() && stk.top().second < number) {
+            pair<int, int> curr = stk.top();
+            stk.pop();
+            nge[curr.first] = number;
+        }
+        stk.push({ index, number });
+    }
+
+    for (int i = 1; i <= N; i++)
+        cout << nge[i] << ' ';
+
+    return 0;
+}
