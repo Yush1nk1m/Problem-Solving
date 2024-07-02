fd37a4f27f316e37419ec215a7a8d0d5850a79fa
Yush1nk1m <kys010306@sogang.ac.kr>
2024-07-02 23:47:42 +0900

    20240702: 14391번 해결
    - 백트래킹으로 접근하여 조각마다 그룹 번호를 매기고 합산할 때 같은 그룹의 조각끼리 더하는 방식으로 구현했으나 실패했다.
    - 비트마스킹 풀이법을 참고하여 0은 오른쪽 방향, 1은 아래쪽 방향으로 뻗어나가는 조각이라고 간주하고 해결하였다.

---
+++ Coding_test/week4/14391_Bitmasking.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define RIGHT 0
+#define DOWN  1
+
+int N, M, maxSum;
+vector<vector<char>> paper;
+vector<vector<int>> segment;
+
+inline void MarkSegment(int mask);
+inline int CalcSum();
+inline void CutCombination();
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+    paper.resize(N, vector<char>(M));
+    segment.resize(N, vector<int>(M));
+    for (int r = 0; r < N; r++)
+        for (int c = 0; c < M; c++) {
+            cin >> paper[r][c];
+            paper[r][c] -= '0';
+        }
+
+    CutCombination();
+    cout << maxSum;
+
+    return 0;
+}
+
+inline void MarkSegment(int mask) {
+    for (int r = 0; r < N; r++)
+        for (int c = 0; c < M; c++) {
+            int index = (r * M) + c;
+            if (mask & (1 << index))
+                segment[r][c] = DOWN;
+            else
+                segment[r][c] = RIGHT;
+        }
+}
+
+inline int CalcSum() {
+    int sum = 0;
+    int visited = 0;
+    for (int r = 0; r < N; r++)
+        for (int c = 0; c < M; c++) {
+            int index = (r * M) + c;
+            if (!(visited & (1 << index))) {
+                visited |= (1 << index);
+                int segsum;
+                // right direction
+                if (segment[r][c] == RIGHT) {
+                    segsum = paper[r][c];
+                    for (int nc = c + 1; nc < M && segment[r][nc] == RIGHT; nc++) {
+                        int nindex = (r * M) + nc;
+                        visited |= (1 << nindex);
+                        segsum *= 10;
+                        segsum += paper[r][nc];
+                    }
+                }
+                // down direction
+                else {
+                    segsum = paper[r][c];
+                    for (int nr = r + 1; nr < N && segment[nr][c] == DOWN; nr++) {
+                        int nindex = (nr * M) + c;
+                        visited |= (1 << nindex);
+                        segsum *= 10;
+                        segsum += paper[nr][c];
+                    }
+                }
+
+                sum += segsum;
+            }
+        }
+    
+    return sum;
+}
+
+inline void CutCombination() {
+    int mask = 0, bound = (1 << (N * M));
+    while (mask < bound) {
+        MarkSegment(mask);
+        maxSum = max(maxSum, CalcSum());
+        mask++;
+    }
+}
