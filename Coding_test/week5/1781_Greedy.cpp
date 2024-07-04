df6ce53d5ed729edb9ede8c3f9a429caaba598e4
Yush1nk1m <kys010306@sogang.ac.kr>
2024-07-04 21:35:10 +0900

    20240704: 1781번 해결

---
+++ Coding_test/week5/1781_Greedy.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file 1781_Greedy.cpp
+ * @brief 00:13:05
+ * @date 2024-07-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+class Problem {
+    public:
+        ll deadline;
+        ll ramen;
+        bool operator<(const Problem& p) {
+            if (deadline == p.deadline)
+                return (ramen < p.ramen);
+            return (deadline < p.deadline);
+        }
+};
+
+ll N, cup;
+vector<Problem> problems;
+priority_queue<ll, vector<ll>, greater<ll>> pq;
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    problems.resize(N);
+    for (int i = 0; i < N; i++)
+        cin >> problems[i].deadline >> problems[i].ramen;
+    sort(problems.begin(), problems.end());
+
+    for (Problem& p : problems) {
+        if (p.deadline <= pq.size())
+            pq.pop();
+        pq.push(p.ramen);
+    }
+
+    while (pq.size()) {
+        cup += pq.top();
+        pq.pop();
+    }
+
+    cout << cup;
+
+    return 0;
+}
