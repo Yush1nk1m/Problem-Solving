e885f24f5c61924c1decf9f19c1c50dc1dc238d8
Yush1nk1m <kys010306@sogang.ac.kr>
2024-07-21 00:48:39 +0900

    20240721: 1450번 해결

--- Coding_test/week0/test.cpp
+++ Coding_test/week0/test.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    map<pair<int, int>, int> m;
-    m[make_pair(0, 0)] = 0;
+    vector<int> v = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
 
-    pair<int, int> key = make_pair(0, 0);
-    cout << (m.find(make_pair(0, 0)) != m.end());
+    int N;
+    cout << "Target number: ";
+    cin >> N;
+
+    cout << "The number of numbers below " << N << ": ";
+    cout << (upper_bound(v.begin(), v.end(), N) - v.begin()) << endl;
 
     return 0;
 }
---
+++ Coding_test/week7/1450_DFS_BinarySearch.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int N, C;
+ll result;
+vector<ll> items;
+vector<ll> sum1, sum2;
+
+void DFS(ll low, ll high, ll sum, vector<ll>& sumv);
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> C;
+
+    items.resize(N);
+    for (int i = 0; i < N; i++)
+        cin >> items[i];
+
+    DFS(0, N/2, 0, sum1);
+    DFS(N/2+1, N-1, 0, sum2);
+
+    sort(sum1.begin(), sum1.end());
+    sort(sum2.begin(), sum2.end());
+
+    for (ll s1 : sum1) {
+        ll remainder = C - s1;
+        result += (upper_bound(sum2.begin(), sum2.end(), remainder) - sum2.begin());
+    }
+
+    cout << result;
+
+    return 0;
+}
+
+void DFS(ll low, ll high, ll sum, vector<ll>& sumv) {
+    if (low > high) {
+        sumv.push_back(sum);
+        return;
+    }
+
+    if (sum + items[low] <= C)
+        DFS(low + 1, high, sum + items[low], sumv);
+    DFS(low + 1, high, sum, sumv);
+}
