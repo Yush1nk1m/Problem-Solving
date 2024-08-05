1f4a967c04ec5d333e6a74a5da468447d904fe3f
Yush1nk1m <kys010306@sogang.ac.kr>
2024-08-05 17:18:11 +0900

    20240805: 24445번 해결

---
+++ BOJ/Graph/24445_BFS.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, R, order = 1;
+vector<vector<int>> graph;
+vector<int> visited;
+queue<int> q;
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M >> R;
+
+    visited.resize(N + 1, 0);
+    graph.resize(N + 1);
+
+    for (int _ = 0; _ < M; _++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for (int i = 1; i <= N; i++)
+        sort(graph[i].rbegin(), graph[i].rend());
+
+    visited[R] = order++;
+    q.push(R);
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+
+        for (int neighbor : graph[curr])
+            if (!visited[neighbor]) {
+                visited[neighbor] = order++;
+                q.push(neighbor);
+            }
+    }
+
+    for (int i = 1; i <= N; i++)
+        cout << visited[i] << '\n';
+
+    return 0;
+}
