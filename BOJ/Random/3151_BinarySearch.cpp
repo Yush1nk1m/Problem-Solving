5e6ec52a08c6b444f450097558f40410abc1faf4
Yush1nk1m <kys010306@sogang.ac.kr>
2024-09-04 16:00:51 +0900

    20240904: 3151번 해결

---
+++ BOJ/Random/3151_BinarySearch.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int N;
+ll cnt;
+vector<int> A;
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    A.resize(N);
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    sort(A.begin(), A.end());
+
+    for (int i1 = 0; i1 < N - 1; i1++)
+        for (int i2 = i1 + 1; i2 < N; i2++) {
+            int target = -(A[i1] + A[i2]);
+            int sidx = lower_bound(A.begin() + i2, A.end(), target) - A.begin();
+            int eidx = upper_bound(A.begin() + i2, A.end(), target) - A.begin();
+            
+            if (sidx != eidx) {
+                cnt += (eidx - sidx - (sidx <= i1 && i1 <= eidx) - (sidx <= i2 && i2 <= eidx));
+            }
+        }
+
+    cout << cnt;
+
+    return 0;
+}
