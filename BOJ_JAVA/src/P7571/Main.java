98c55f5e68ac2ad353f3bdc1dd7b34f4d99ef87b
Yush1nk1m <kys010306@sogang.ac.kr>
2024-10-08 19:39:46 +0900

    20241008: 7571번 해결

---
+++ BOJ_JAVA/src/P7571/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class P7571_Math {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        List<Integer> r = new ArrayList<>(M);
+        List<Integer> c = new ArrayList<>(M);
+
+        for (int i = 0; i < M; i++) {
+            r.add(sc.nextInt());
+            c.add(sc.nextInt());
+        }
+
+        r.sort(null);
+        c.sort(null);
+
+        int rMid = r.get(M / 2);
+        int cMid = c.get(M / 2);
+
+        Long result = 0L;
+        for (int i = 0; i < M; i++) {
+            result += Math.abs(rMid - r.get(i)) + Math.abs(cMid - c.get(i));
+        }
+
+        System.out.println(result);
+    }
+}
