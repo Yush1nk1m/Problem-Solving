47fa24b022b044236227f478b17fc577531abef9
Yush1nk1m <kys010306@sogang.ac.kr>
2024-10-25 03:47:52 +0900

    20241025: 백준 19538번 최적화

---
+++ BOJ/Random/19538_BFS.cpp
@@ -0,0 +1,84 @@
+/**
+ * @file 19538_BFS.cpp
+ * @brief 00:25:06
+ * @date 2024-10-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NONE    -1
+
+int N, M, t;
+vector<vector<int>> graph;
+vector<int> times;
+queue<int> q;
+
+inline bool canBelieve(int node);
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    graph.resize(N + 1);
+    times.resize(N + 1, NONE);
+
+    for (int i = 1; i <= N; i++) {
+        int neighbor;
+        while (cin >> neighbor) {
+            if (neighbor == 0)
+                break;
+            graph[i].push_back(neighbor);
+        }
+    }
+
+    cin >> M;
+    while (M--) {
+        int trigger;
+        cin >> trigger;
+        times[trigger] = 0;
+        q.push(trigger);
+    }
+
+    while (!q.empty()) {
+        t++;
+        
+        int qsize = q.size();
+        vector<int> candidates;
+
+        while (qsize--) {
+            int curr = q.front();
+            q.pop();
+            
+            for (int neighbor : graph[curr])
+                if (times[neighbor] == NONE && canBelieve(neighbor)) {
+                    candidates.push_back(neighbor);
+                    q.push(neighbor);
+                }
+        }
+
+        for (int c : candidates)
+            times[c] = t;
+    }
+
+    for (int i = 1; i <= N; i++)
+        cout << times[i] << ' ';
+    
+    return 0;
+}
+
+inline bool canBelieve(int node) {
+    int cnt = 0;
+    for (int neighbor : graph[node])
+        if (times[neighbor] != NONE)
+            cnt++;
+    
+    if (graph[node].size() % 2 == 0)
+        return graph[node].size() / 2 <= cnt;
+    else
+        return graph[node].size() / 2 < cnt;
+}
--- BOJ/Random/19538_OPT.cpp
+++ BOJ/Random/19538_OPT.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int N, M, t;
 vector<vector<int>> graph;
+vector<int> inDegree;
 vector<int> times;
 queue<int> q;
 
-inline bool canBelieve(int node);
+inline bool canBelieve(int neighbor);
 
 int main(int argc, char* argv[]) {
     ios_base::sync_with_stdio(false);
@@ -16,8 +17,10 @@ int main(int argc, char* argv[]) {
     cout.tie(NULL);
 
     cin >> N;
+    
     graph.resize(N + 1);
     times.resize(N + 1, NONE);
+    inDegree.resize(N + 1, 0);
 
     for (int i = 1; i <= N; i++) {
         int neighbor;
@@ -38,39 +41,32 @@ int main(int argc, char* argv[]) {
 
     while (!q.empty()) {
         t++;
-        
-        int qsize = q.size();
-        vector<int> candidates;
 
+        int qsize = q.size();
         while (qsize--) {
             int curr = q.front();
             q.pop();
-            
+
             for (int neighbor : graph[curr])
                 if (times[neighbor] == NONE && canBelieve(neighbor)) {
-                    candidates.push_back(neighbor);
+                    times[neighbor] = t;
                     q.push(neighbor);
                 }
         }
-
-        for (int c : candidates)
-            times[c] = t;
     }
 
     for (int i = 1; i <= N; i++)
         cout << times[i] << ' ';
-    
+
     return 0;
 }
 
-inline bool canBelieve(int node) {
-    int cnt = 0;
-    for (int neighbor : graph[node])
-        if (times[neighbor] != NONE)
-            cnt++;
-    
-    if (graph[node].size() % 2 == 0)
-        return graph[node].size() / 2 <= cnt;
+inline bool canBelieve(int neighbor) {
+    int gsize = graph[neighbor].size();
+    inDegree[neighbor]++;
+
+    if (gsize % 2 == 0)
+        return gsize / 2 <= inDegree[neighbor];
     else
-        return graph[node].size() / 2 < cnt;
+        return gsize / 2 < inDegree[neighbor];
 }
