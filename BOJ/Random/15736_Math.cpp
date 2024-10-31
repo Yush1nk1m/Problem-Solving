449821505239f73626003e8f3d8f2b7f524b61ae
Yush1nk1m <kys010306@sogang.ac.kr>
2024-10-31 11:58:04 +0900

    20241031: 15736번 해결

---
+++ BOJ/Random/15736_Math.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file 15736_Math.cpp
+ * @brief 00:16:27
+ * @date 2024-10-31
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll N;
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    cout << floor(sqrt(N));
+
+    return 0;
+}
