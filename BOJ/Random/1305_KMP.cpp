5d110bae61232a73c7f12f85e80f7f296e51676a
Yush1nk1m <kys010306@sogang.ac.kr>
2024-12-01 16:25:09 +0900

    20241201: 1305번 해결

---
+++ BOJ/Random/1305_KMP.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int L;
+string line;
+vector<int> failure;
+
+inline vector<int> getFailure(string& pattern);
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> L >> line;
+    failure = getFailure(line);
+        
+    cout << L - failure[L-1];
+
+    return 0;
+}
+
+inline vector<int> getFailure(string& pattern) {
+    vector<int> failure(L, 0);
+
+    int i = 1, j = 0;
+    while (i < L) {
+        if (pattern[i] == pattern[j]) {
+            failure[i] = j + 1;
+            i++;
+            j++;
+        } else if (j > 0) {
+            j = failure[j-1];
+        } else {
+            i++;
+        }
+    }
+
+    return failure;
+}
