c2dd09a8eaf32b0a1c12723cab9a4d6ff89d6c8f
Yush1nk1m <kys010306@sogang.ac.kr>
2025-08-24 23:01:05 +0900

    20250824

--- BOJ_JAVA/src/P1926/Main.java
+++ BOJ_JAVA/src/P1926/Main.java
@@ -1,4 +1,94 @@
 package P1926;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int n, m;
+    static int[][] paper;
+    static boolean[][] visited;
+    static int picNum = 0, maxArea = 0;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        paper = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int r = 0; r < n; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < m; c++)
+                paper[r][c] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int r = 0; r < n; r++)
+            for (int c = 0; c < m; c++)
+                if (!visited[r][c] && paper[r][c] == 1) {
+                    picNum++;
+                    maxArea = Math.max(maxArea, bfs(r, c));
+                }
+
+        bw.write(String.valueOf(picNum));
+        bw.write('\n');
+        bw.write(String.valueOf(maxArea));
+
+        br.close();
+        bw.close();
+    }
+
+    public static int bfs(int r, int c) {
+        class Coords {
+            int r, c;
+
+            public Coords(int r, int c) {
+                this.r = r;
+                this.c = c;
+            }
+
+            public int getR() {
+                return r;
+            }
+
+            public int getC() {
+                return c;
+            }
+
+            public List<Coords> getNeighbors() {
+                List<Coords> result = new ArrayList<>();
+                int[] dr = new int[]{-1, 0, 1, 0};
+                int[] dc = new int[]{0, -1, 0, 1};
+
+                for (int i = 0; i < 4; i++) {
+                    int nr = r + dr[i], nc = c + dc[i];
+                    if (0 <= nr && nr < n && 0 <= nc && nc < m && paper[nr][nc] == 1)
+                        result.add(new Coords(nr, nc));
+                }
+
+                return result;
+            }
+        }
+        Queue<Coords> queue = new ArrayDeque<>();
+        int size = 0;
+
+        size++;
+        visited[r][c] = true;
+        queue.offer(new Coords(r, c));
+        while (!queue.isEmpty()) {
+            Coords curr = queue.poll();
+
+            for (Coords next : curr.getNeighbors())
+                if (!visited[next.getR()][next.getC()]) {
+                    size++;
+                    visited[next.getR()][next.getC()] = true;
+                    queue.offer(next);
+                }
+        }
+
+        return size;
+    }
 }
--- BOJ_JAVA/src/P5587/Main.java
+++ BOJ_JAVA/src/P5587/Main.java
@@ -1,4 +1,57 @@
 package P5587;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        boolean[] myCard = new boolean[n * 2 + 1];
+        List<Integer> c1 = new LinkedList<>();
+        List<Integer> c2 = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            myCard[Integer.parseInt(br.readLine())] = true;
+        }
+
+        for (int i = 1; i <= n * 2; i++) {
+            if (myCard[i])
+                c1.add(i);
+            else
+                c2.add(i);
+        }
+
+        boolean turn = false;
+        int currCard = c1.get(0);
+        c1.remove(0);
+        Outer: while (!c1.isEmpty() && !c2.isEmpty()) {
+            if (turn) {
+                turn = false;
+                for (int i = 0; i < c1.size(); i++)
+                    if (c1.get(i) > currCard) {
+                        currCard = c1.get(i);
+                        c1.remove(i);
+                        continue Outer;
+                    }
+            } else {
+                turn = true;
+                for (int i = 0; i < c2.size(); i++)
+                    if (c2.get(i) > currCard) {
+                        currCard = c2.get(i);
+                        c2.remove(i);
+                        continue Outer;
+                    }
+            }
+            currCard = 0;
+        }
+
+        bw.write(String.valueOf(c2.size()));
+        bw.write('\n');
+        bw.write(String.valueOf(c1.size()));
+        br.close();
+        bw.close();
+    }
 }
