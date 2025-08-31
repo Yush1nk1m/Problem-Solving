327b1a9b406435ae3b4bedc9f061627904cf04db
Yush1nk1m <kys010306@sogang.ac.kr>
2025-08-31 16:14:02 +0900

    20250831

--- BOJ_JAVA/src/P16571/Main.java
+++ BOJ_JAVA/src/P16571/Main.java
@@ -1,4 +1,129 @@
 package P16571;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static class Coords {
+        int r, c;
+
+        public Coords(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        public int getR() {
+            return r;
+        }
+
+        public int getC() {
+            return c;
+        }
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static final int EMPTY = 0;
+    static final int X = 1;
+    static final int O = 2;
+    static final int NONE = 3;
+    static final int LOSE = 0;
+    static final int DRAW = 1;
+    static final int WIN = 2;
+
+    static int player;
+    static int[][] board = new int[3][3];
+    static List<Coords> empties = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        for (int r = 0; r < 3; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < 3; c++) {
+                board[r][c] = Integer.parseInt(st.nextToken());
+                if (board[r][c] == EMPTY)
+                    empties.add(new Coords(r, c));
+            }
+        }
+        player = getTurn();
+
+        switch (dfs(0, player)) {
+            case LOSE:
+                System.out.print("L");
+                break;
+            case DRAW:
+                System.out.print("D");
+                break;
+            case WIN:
+                System.out.print("W");
+                break;
+        }
+    }
+
+    public static int getTurn() {
+        int x = 0, o = 0;
+        for (int r = 0; r < 3; r++)
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == X)
+                    x++;
+                else if (board[r][c] == O)
+                    o++;
+            }
+        return x == o ? X : O;
+    }
+
+    public static int getWinner() {
+        for (int i = 0; i < 3; i++) {
+            // ith row
+            if (board[i][0] == X && board[i][1] == X && board[i][2] == X)
+                return X;
+            if (board[i][0] == O && board[i][1] == O && board[i][2] == O)
+                return O;
+            // ith col
+            if (board[0][i] == X && board[1][i] == X && board[2][i] == X)
+                return X;
+            if (board[0][i] == O && board[1][i] == O && board[2][i] == O)
+                return O;
+        }
+        // right-lower diagonal
+        if (board[0][0] == X && board[1][1] == X && board[2][2] == X)
+            return X;
+        if (board[0][0] == O && board[1][1] == O && board[2][2] == O)
+            return O;
+        // right-upper diagonal
+        if (board[2][0] == X && board[1][1] == X && board[0][2] == X)
+            return X;
+        if (board[2][0] == O && board[1][1] == O && board[0][2] == O)
+            return O;
+
+        return NONE;
+    }
+
+    public static int dfs(int mask, int turn) {
+        int winner;
+        if (mask == (1 << empties.size()) - 1) {
+            winner = getWinner();
+            return winner == NONE ? DRAW : winner == turn ? WIN : LOSE;
+        } else if ((winner = getWinner()) != NONE) {
+            return winner == turn ? WIN : LOSE;
+        }
+
+        int result = NONE;
+        for (int i = 0; i < empties.size(); i++)
+            if ((mask & (1 << i)) == 0) {
+                mask |= (1 << i);
+                Coords coords = empties.get(i);
+                board[coords.getR()][coords.getC()] = turn;
+                result = Math.min(result, dfs(mask, turn == X ? O : X));
+                board[coords.getR()][coords.getC()] = EMPTY;
+                mask ^= (1 << i);
+            }
+
+        return switch (result) {
+            case WIN -> LOSE;
+            case LOSE -> WIN;
+            default -> DRAW;
+        };
+    }
 }
--- BOJ_JAVA/src/P31230/Main.java
+++ BOJ_JAVA/src/P31230/Main.java
@@ -1,4 +1,126 @@
 package P31230;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static class Edge implements Comparable<Edge> {
+        int src, dest;
+        long cost;
+
+        public Edge(int src, int dest, long cost) {
+            this.src = src;
+            this.dest = dest;
+            this.cost = cost;
+        }
+
+        public int getSrc() {
+            return src;
+        }
+
+        public int getDest() {
+            return dest;
+        }
+
+        public long getCost() {
+            return cost;
+        }
+
+        public int compareTo(Edge other) {
+            return Long.compare(cost, other.cost);
+        }
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static final long INF = Long.MAX_VALUE;
+
+    static int N, M, A, B;
+    static List<List<Edge>> graph = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= N; i++)
+            graph.add(new ArrayList<>());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int src = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(src).add(new Edge(src, dest, cost));
+            graph.get(dest).add(new Edge(dest, src, cost));
+        }
+
+        Map<Integer, List<Integer>> parents = dijkstra();
+        List<Integer> result = findShortestPathNodes(parents);
+        sb.append(result.size()).append('\n');
+        for (int node : result)
+            sb.append(node).append(' ');
+        System.out.println(sb.toString());
+    }
+
+    public static Map<Integer, List<Integer>> dijkstra() {
+        long[] distance = new long[N + 1];
+        Map<Integer, List<Integer>> parents = new HashMap<>();
+        Arrays.fill(distance, INF);
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        distance[A] = 0;
+        pq.offer(new Edge(A, A, 0));
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int src = edge.getSrc(), mid = edge.getDest();
+            long midCost = edge.getCost();
+            if (distance[mid] < midCost)
+                continue;
+
+            for (Edge nextEdge : graph.get(mid)) {
+                int dest = nextEdge.getDest();
+                long destCost = nextEdge.getCost();
+                if (midCost + destCost < distance[dest]) {
+                    distance[dest] = midCost + destCost;
+                    parents.put(dest, new ArrayList<>());
+                    parents.get(dest).add(mid);
+                    pq.offer(new Edge(src, dest, distance[dest]));
+                } else if (midCost + destCost == distance[dest]) {
+                    parents.put(dest, parents.getOrDefault(dest, new ArrayList<>()));
+                    parents.get(dest).add(mid);
+                }
+            }
+        }
+
+        return parents;
+    }
+
+    public static List<Integer> findShortestPathNodes(Map<Integer, List<Integer>> parents) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        set.add(B);
+        queue.offer(B);
+        list.add(B);
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            if (curr == A) {
+                continue;
+            }
+
+            for (int parent : parents.get(curr))
+                if (!set.contains(parent)) {
+                    set.add(parent);
+                    queue.add(parent);
+                    list.add(parent);
+                }
+        }
+
+        Collections.sort(list);
+        return list;
+    }
 }
