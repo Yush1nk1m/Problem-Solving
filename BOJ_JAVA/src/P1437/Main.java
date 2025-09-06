46cae4c72e850ae3d11140a4cbe2621955ef64b5
Yush1nk1m <kys010306@sogang.ac.kr>
2025-09-07 00:35:59 +0900

    20250907

--- BOJ_JAVA/src/P1437/Main.java
+++ BOJ_JAVA/src/P1437/Main.java
@@ -1,4 +1,24 @@
 package P1437;
 
+import java.io.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final long MOD = 10_007L;
+    static long N;
+
+    public static void main(String[] args) throws IOException {
+        N = Long.parseLong(br.readLine());
+        long sol = 1;
+        while (4 < N) {
+            N -= 3;
+            sol *= 3;
+            sol %= MOD;
+        }
+        sol *= N;
+        sol %= MOD;
+        System.out.println(sol);
+    }
+
 }
