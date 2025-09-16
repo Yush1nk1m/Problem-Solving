fbde7a53f7d59f6c5d1c59d64df7ed3af8e355e8
Yush1nk1m <kys010306@sogang.ac.kr>
2025-09-16 20:39:06 +0900

    20250916: 백준 1문제 해결

--- BOJ_JAVA/src/P2631/Main.java
+++ BOJ_JAVA/src/P2631/Main.java
@@ -1,4 +1,29 @@
 package P2631;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static int[] arr;
+    static List<Integer> LIS = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        LIS.add(arr[0]);
+        for (int i = 1; i < N; i++) {
+            int j = -Collections.binarySearch(LIS, arr[i]) - 1;
+            if (LIS.size() == j)
+                LIS.add(arr[i]);
+            else
+                LIS.set(j, arr[i]);
+        }
+        System.out.println(N - LIS.size());
+    }
 }
