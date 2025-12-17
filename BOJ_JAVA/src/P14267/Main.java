c7791e2e61c8f987de2c32f47d1d738f69a1e6fa
Yush1nk1m <kys010306@sogang.ac.kr>
2025-12-17 22:04:47 +0900

    20251217: BOJ 2문제 해결

--- BOJ_JAVA/src/P14267/Main.java
+++ BOJ_JAVA/src/P14267/Main.java
@@ -1,4 +1,45 @@
 package P14267;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String[] tokens;
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static int[] w, compliment;
+    static List<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        tokens = br.readLine().split(" ");
+        n = Integer.parseInt(tokens[0]);
+        m = Integer.parseInt(tokens[1]);
+        w = new int[n + 1];
+        compliment = new int[n + 1];
+        graph = new List[n + 1];
+        for (int i = 1; i <= n; ++i)
+            graph[i] = new ArrayList<>();
+        tokens = br.readLine().split(" ");
+        for (int i = 2; i <= n; ++i) {
+            int j = Integer.parseInt(tokens[i - 1]);
+            graph[j].add(i);
+        }
+        while (m-- > 0) {
+            tokens = br.readLine().split(" ");
+            w[Integer.parseInt(tokens[0])] += Integer.parseInt(tokens[1]);
+        }
+        DFS(1, 0);
+        for (int i = 1; i <= n; ++i)
+            sb.append(compliment[i]).append(' ');
+        System.out.println(sb);
+    }
+
+    public static void DFS(int root, int c) {
+        compliment[root] = c + w[root];
+        for (int next : graph[root])
+            DFS(next, c + w[root]);
+    }
 }
--- BOJ_JAVA/src/P24453/Main.java
+++ BOJ_JAVA/src/P24453/Main.java
@@ -1,4 +1,55 @@
 package P24453;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String[] tokens;
+
+    static int N, M, X, Y, gap;
+    static int[] error;
+
+    public static void main(String[] args) throws IOException {
+        tokens = br.readLine().split(" ");
+        N = Integer.parseInt(tokens[0]);
+        M = Integer.parseInt(tokens[1]);
+        error = new int[M + 2];
+        error[M + 1] = N + 1;
+        tokens = br.readLine().split(" ");
+        for (int i = 1; i <= M; ++i) {
+            error[i] = Integer.parseInt(tokens[i - 1]);
+            gap = Math.max(gap, error[i] - error[i - 1] - 1);
+        }
+        tokens = br.readLine().split(" ");
+        X = Integer.parseInt(tokens[0]);
+        Y = Integer.parseInt(tokens[1]);
+        if (gap >= X) {
+            System.out.println(M - Y);
+            return;
+        }
+        System.out.println(M - solve());
+    }
+
+    public static int solve() {
+        int l = Y, h = M, ret = M;
+        while (l <= h) {
+            int m = (l + h) >> 1;
+            if (validate(m) >= X) {
+                ret = Math.min(ret, m);
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ret;
+    }
+
+    public static int validate(int size) {
+        int g = 0;
+        for (int i = 1; i < error.length - size; ++i)
+            g = Math.max(g, error[i + size] - error[i - 1] - 1);
+        return g;
+    }
 }
