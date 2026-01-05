4708b31ac58c1d9fd87f3ba79aa122b8abdf102b
Yush1nk1m <kys010306@sogang.ac.kr>
2026-01-05 22:44:45 +0900

    20260105: BOJ 2문제 해결

--- BOJ_JAVA/src/P12355/Main.java
+++ BOJ_JAVA/src/P12355/Main.java
@@ -1,4 +1,53 @@
 package P12355;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static String[] tokens;
+
+    static int T, N;
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; ++i) {
+            N = Integer.parseInt(br.readLine());
+            tokens = br.readLine().split(" ");
+            sb.append("Case #").append(i).append(": ").append(solve()).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static int solve() {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+
+        for (String token : tokens) {
+            int number = Integer.parseInt(token);
+            int index = binarySearch(list, number);
+            if (index == list.size())
+                list.add(number);
+            else
+                list.set(index, number);
+        }
+
+        return N - list.size() + 1;
+    }
+
+    public static int binarySearch(List<Integer> list, int target) {
+        int l = 0, h = list.size() - 1, ret = list.size();
+        while (l <= h) {
+            int m = (l + h) >> 1;
+            if (list.get(m) >= target) {
+                ret = Math.min(ret, m);
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ret;
+    }
 }
--- BOJ_JAVA/src/P33705/Main.java
+++ BOJ_JAVA/src/P33705/Main.java
@@ -1,4 +1,37 @@
 package P33705;
 
+import java.io.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String[] tokens;
+
+    static int N, result = 2;
+    static int[] sum;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        sum = new int[N + 1];
+        tokens = br.readLine().split(" ");
+        for (int i = 0; i < N; ++i) {
+            sum[i + 1] = tokens[i].equals("1") ? 1 : 0;
+            sum[i + 1] += sum[i];
+        }
+
+        if (electable(sum[N] - sum[0], N))
+            result = 0;
+        if (result == 2)
+            for (int i = 0; i < N; i++)
+                if (electable(sum[N] - sum[i], N - i) ||
+                        electable(sum[N - i] - sum[0], N - i)) {
+                    result = 1;
+                    break;
+                }
+        System.out.print(result);
+    }
+
+    public static boolean electable(int voter, int total) {
+        return voter >= (total + 1) >> 1;
+    }
 }
