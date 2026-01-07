e29833beecaabc89c86ca2ffa21a383e48999401
Yush1nk1m <kys010306@sogang.ac.kr>
2026-01-08 00:00:19 +0900

    20260108: BOJ 2문제 해결

--- BOJ_JAVA/src/P21236/Main.java
+++ BOJ_JAVA/src/P21236/Main.java
@@ -1,4 +1,47 @@
 package P21236;
 
+import java.io.*;
+
 public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String[] tokens;
+    static StringBuilder sb = new StringBuilder();
+
+    static final int[] dx = {-1, 0, 1, 0};
+    static final int[] dy = {0, -1, 0, 1};
+
+    static int N, count;
+    static int[][] placed = new int[1001][1001];
+    static int[][] adjacent = new int[1001][1001];
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            tokens = br.readLine().split(" ");
+            int x = Integer.parseInt(tokens[0]);
+            int y = Integer.parseInt(tokens[1]);
+            placed[x][y] = 1;
+            for (int d = 0; d < 4; ++d) {
+                int nx = x + dx[d], ny = y + dy[d];
+                if (!isValidPoint(nx, ny))
+                    continue;
+                ++adjacent[nx][ny];
+                if (placed[nx][ny] == 0)
+                    continue;
+                if (adjacent[nx][ny] == 3)
+                    ++count;
+                else if (adjacent[nx][ny] == 4)
+                    --count;
+            }
+            if (adjacent[x][y] == 3)
+                ++count;
+            sb.append(count).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static boolean isValidPoint(int x, int y) {
+        return 0 <= x && x <= 1000 && 0 <= y && y <= 1000;
+    }
 }
--- BOJ_JAVA/src/P2871/Main.java
+++ BOJ_JAVA/src/P2871/Main.java
@@ -1,4 +1,63 @@
 package P2871;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
+
+    static class Node implements Comparable<Node> {
+        int index;
+        boolean used;
+        char character;
+
+        public Node(int index, char character) {
+            this.index = index;
+            this.used = false;
+            this.character = character;
+        }
+
+        public int compareTo(Node other) {
+            if (character == other.character)
+                return Integer.compare(other.index, index);
+            return Character.compare(character, other.character);
+        }
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, turn;
+    static char[] line;
+    static Deque<Node> deque = new ArrayDeque<>();
+    static PriorityQueue<Node> pq = new PriorityQueue<>();
+    static StringBuilder[] builders = new StringBuilder[]{new StringBuilder(), new StringBuilder()};
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        line = br.readLine().toCharArray();
+        for (int i = 0; i < N; ++i) {
+            deque.offerLast(new Node(i, line[i]));
+            pq.offer(deque.peekLast());
+        }
+        while (N-- > 0) {
+            builders[turn].append(((turn == 0) ? pollFromDeque() : pollFromPriorityQueue()));
+            turn = 1 - turn;
+        }
+        sb.append((builders[0].toString().compareTo(builders[1].toString()) > 0 ? "DA\n" : "NE\n")).append(builders[1].toString());
+        System.out.print(sb);
+    }
+
+    public static char pollFromDeque() {
+        while (deque.peekLast().used)
+            deque.pollLast();
+        deque.peekLast().used = true;
+        return deque.pollLast().character;
+    }
+
+    public static char pollFromPriorityQueue() {
+        while (pq.peek().used)
+            pq.poll();
+        pq.peek().used = true;
+        return pq.poll().character;
+    }
 }
